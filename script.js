5567c91e75fe44ac45b2c78b3015170e4237aa95
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2023-10-26 17:53:28 -0400

    Create canvas using DOM

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello World')
+let canvas = document.querySelector('.canvas');
+canvas.style.gridTemplateColumns = 'repeat(16, 1fr)';
+canvas.style.gridTemplateRows = 'repeat(16, 1fr)';
+
+for (let i = 0; i < 256; i++) {
+    let cell = document.createElement('div');
+    cell.style.backgroundColor = 'gray';
+    canvas.insertAdjacentElement('beforeend', cell)
+}
