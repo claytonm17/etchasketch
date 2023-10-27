ea59c14fa6dee1664ab5947f77d343a39e552705
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2023-10-27 09:31:54 -0400

    Added functionality to the etch-a-sketch and a clear button

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
+// Creating canvas for etch-a-sketch
 let canvas = document.querySelector('.canvas');
 canvas.style.gridTemplateColumns = 'repeat(16, 1fr)';
 canvas.style.gridTemplateRows = 'repeat(16, 1fr)';
 
+// Inital creation of canvas
 for (let i = 0; i < 256; i++) {
     let cell = document.createElement('div');
-    cell.style.backgroundColor = 'gray';
+    cell.style.backgroundColor = 'white';
+    cell.className = 'cell';
     canvas.insertAdjacentElement('beforeend', cell)
-}
+};
+
+// Assigning cells
+const cells = document.querySelectorAll('.cell');
+
+// Assigning clear button
+const clear = document.getElementById('clear-button');
+
+// Actual etch-a-sketch functionality
+cells.forEach(cell => {
+    cell.addEventListener('mouseover', () => {
+        cell.style.backgroundColor = 'black';
+    });
+});
+
+// Event listener for when clear button is clicked
+clear.addEventListener('click', () => {
+    cells.forEach(cell => {
+        cell.style.backgroundColor = 'white';
+    });
+});
