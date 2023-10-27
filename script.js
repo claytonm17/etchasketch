48eea11741b7d131bf8e09b4f90b2b447537d69b
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2023-10-27 11:18:41 -0400

    Add functionality to change canvas size

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
-// Creating canvas for etch-a-sketch
-let canvas = document.querySelector('.canvas');
-canvas.style.gridTemplateColumns = 'repeat(16, 1fr)';
-canvas.style.gridTemplateRows = 'repeat(16, 1fr)';
+// Initial draw color set to black
+let drawColor = 'black';
 
-// Inital creation of canvas
-for (let i = 0; i < 256; i++) {
-    let cell = document.createElement('div');
-    cell.style.backgroundColor = 'white';
-    cell.className = 'cell';
-    canvas.insertAdjacentElement('beforeend', cell)
+// Initial canvas size
+let size = 16
+
+// Allowing for asjustment of canvas size
+const sizeInput = document.getElementById('size');
+const setSize = document.getElementById('set-size');
+
+let userValue = '';
+
+sizeInput.addEventListener('input', (event) => {
+    userValue = parseInt(event.target.value, 10);
+});
+
+setSize.addEventListener('click', () => {
+    deleteCanvas();
+    createCanvas(userValue);
+});
+
+function deleteCanvas() {
+    const elements = document.getElementsByClassName('cell');
+    while (elements.length > 0) {
+        elements[0].parentNode.removeChild(elements[0]);
+    };
 };
+
+function createCanvas(size) {
+    // Creating canvas for etch-a-sketch
+    let canvas = document.querySelector('.canvas');
+    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    // Inital creation of canvas
+    for (let i = 0; i < (size*size); i++) {
+        let cell = document.createElement('div');
+        cell.style.backgroundColor = 'white';
+        cell.className = 'cell';
+        canvas.insertAdjacentElement('beforeend', cell)
+    };
+};
+
+// Building canvas
+createCanvas(size);
 
 // Assigning cells
 const cells = document.querySelectorAll('.cell');
@@ -26,13 +59,14 @@ cells.forEach(cell => {
 
 // Event listener for when clear button is clicked
 clear.addEventListener('click', () => {
+    clearCanvas();
+});
+
+function clearCanvas() {
     cells.forEach(cell => {
         cell.style.backgroundColor = 'white';
     });
-});
-
-// Initial draw color set to black
-let drawColor = 'black';
+};
 
 // Colored buttons assignment
 const black = document.getElementById('black-button');
@@ -57,6 +91,5 @@ let randomColor = ('#' + randomHex.slice(0,6));
 return randomColor;
 };
 function randomColorSet() {
-    let randomColorGen = randomColorGeneration();
-    drawColor = randomColorGen;
+    drawColor = randomColorGeneration();
 };
